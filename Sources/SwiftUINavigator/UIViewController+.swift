16849f4b8946936818bf5028e2a0a0164b8173c8
insub <vb15@kakao.com>
2023-10-12 01:21:15 +0900

    Create UIViewController+.swift

---
+++ Sources/SwiftUINavigator/UIViewController+.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+.swift
+//
+//
+//  Created by 김인섭 on 10/12/23.
+//
+
+import UIKit
+
+public extension UIViewController {
+    
+    func addChildAndSubView(_ controller: UIViewController) {
+        self.addChild(controller)
+        controller.view.frame = self.view.frame
+        self.view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+    }
+    
+    func removeFromParentAndSuperView() {
+        self.willMove(toParent: nil)
+        self.view.removeFromSuperview()
+        self.removeFromParent()
+    }
+}
