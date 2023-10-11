2b4486d1dc19c437dd380e5f882661b1fcdf4dbf
insub <vb15@kakao.com>
2023-10-12 01:21:18 +0900

    Create Wrappable.swift

---
+++ Sources/SwiftUINavigator/Wrappable.swift
@@ -0,0 +1,28 @@
+//
+//  Wrappable.swift
+//
+//
+//  Created by 김인섭 on 10/12/23.
+//
+
+import SwiftUI
+
+public protocol Wrappable: View {
+    
+    var navigator: Navigator? { get set }
+}
+
+public extension Wrappable {
+    
+    func asViewController(
+        backgroundColor: UIColor? = nil,
+        hideBottomBar: Bool = true
+    ) -> WrapperViewController<Self> {
+        
+        .init(
+            content: self,
+            backgroundColor: backgroundColor,
+            hideBottomBar: hideBottomBar
+        )
+    }
+}
